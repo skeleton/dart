2632a5c40019da42203bfa9f5e2f494a9a38633b
skeleton <marie.bochu@akeneo.com>
2019-03-16 23:52:34 +0100

    Save a new party through the handler

--- src/Application/Party/NewPartyHandler.php
+++ src/Application/Party/NewPartyHandler.php
@@ -5,12 +5,21 @@ namespace Skeleton\Dart\Application\Party;
 use Ramsey\Uuid\Uuid;
 use Skeleton\Dart\Domain\Party\Identifier;
 use Skeleton\Dart\Domain\Party\Party;
+use Skeleton\Dart\Domain\Party\PartyRepository;
 use Skeleton\Dart\Domain\Party\Type;
 use Skeleton\Dart\Domain\Party\Player;
 use Skeleton\Dart\Domain\Party\PlayersCollection;
 
 class NewPartyHandler
 {
+    /** @var PartyRepository */
+    private $partyRepository;
+
+    public function __construct(PartyRepository $partyRepository)
+    {
+        $this->partyRepository = $partyRepository;
+    }
+
     public function __invoke(NewParty $newParty)
     {
         $type = Type::fromString($newParty->type);
@@ -23,5 +32,6 @@ class NewPartyHandler
         $players = PlayersCollection::fromPlayers($players);
         $identifier = Identifier::fromString(Uuid::uuid4()->toString());
         $party = Party::play($identifier, $type, $players);
+        $this->partyRepository->save($party);
     }
 }

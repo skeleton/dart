11d8ddbbb1b0d69f8e5143ab6a4d1bf397d9f10e
skeleton <marie.bochu@akeneo.com>
2019-03-18 18:15:31 +0100

    Add play-party vue component

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,4 @@
-// 0. If using a module system, call Vue.use(VueRouter)
-
-// 1. Define route components.
-// These can be imported from other files
-const NewParty = Vue.component('new-party', {
+const newParty = Vue.component('new-party', {
     data: function () {
         return {
             game: null,
@@ -32,7 +28,9 @@ const NewParty = Vue.component('new-party', {
 
             axios
                 .post('/new-party', params)
-                .then(response => {})
+                .then(response => {
+                    router.push('/play-party')
+                })
                 .catch(e => {
                     console.log(e);
                 })
@@ -40,27 +38,23 @@ const NewParty = Vue.component('new-party', {
     }
 })
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
-const routes = [
-    { path: '/new-party', component: NewParty },
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = new VueRouter({
-    routes
+const playParty = Vue.component('play-party', {
+    data: function () {
+        return {
+        }
+    },
+    template: '#play-party',
+    methods: {
+    }
 })
 
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
+const routes = [
+    { path: '/new-party', component: newParty },
+    { path: '/play-party', component: playParty },
+]
+
+const router = new VueRouter({routes})
+
 const app = new Vue({
     router
 }).$mount('#app')
-
-// Now the app has started!
